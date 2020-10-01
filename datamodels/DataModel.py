612030cc9860b238cb44e4a397d63ae0416ec1f8
Peiyi Wang <42565075+Wangpeiyi9979@users.noreply.github.com>
2020-10-01 13:29:26 +0800

    dataset -> datamodels 原始dataset用来存放数据

--- datamodels/DataModel.py
+++ datamodels/DataModel.py
@@ -1,90 +1,12 @@
-import codecs
 import os
-import random
-
 import numpy as np
-from texttable import Texttable
-from ipdb import set_trace
-from pytorch_pretrained_bert import BertTokenizer
 from torch.utils.data import Dataset
-
-
-class DataProcessor(object):
-    """Base class for data converters for sequence classification data sets."""
-    def __init__(self, opt):
-        self.opt = opt
-        all_labels = self.get_labels()
-        self.id2label = {i: j for i, j in enumerate(all_labels)}
-        self.label2id = {j: i for i, j in enumerate(all_labels)}
-        self.tokenization = BertTokenizer.from_pretrained(self.opt.vocab_dir)
-
-    def get_train_examples(self):
-        return self._create_example(
-            self._read_data(os.path.join(self.opt.data_dir, "train"))
-        )
-
-    def get_dev_examples(self):
-        return self._create_example(
-            self._read_data(os.path.join(self.opt.data_dir, 'dev'))
-        )
-
-    def get_test_examples(self):
-        return self._create_example(
-            self._read_data(os.path.join(self.opt.data_dir, "test"))
-        )
-
-    def get_labels(self, data_dir=None):
-        """
-        此处填写代码
-        """
-        return ["O", "B-ORG", "I-ORG", "B-COMP", "I-COMP", "X"]
-
-    def _read_data(self, input_file):
-        """
-        此处填写代码
-        """
-        return data
-
-    def _create_single_example(self, param):
-        """处理一个样例数据为数字表示
-        param:
-        return:
-        """
-        return x1, x2, x3
-
-    def _create_example(self, lines):
-        x1s = []
-        x2s = []
-        x3s = []
-        for idx, (labels, text) in enumerate(lines):
-            x1, x2, x3 = self._create_single_example(text, labels)
-            x1s.append(x1)
-            x2s.append(x2)
-            x3s.append(x3)
-            if idx == random.randint(0, 1000):
-                print("Judge data...")
-                print("************************")
-                t = Texttable(max_width=self.opt.max_length)
-                length = min(len(text), 24)   # 显示前20个字符即可
-                t.add_row(['text:'] + text[:length])
-                t.add_row(['labels:'] + labels[:length])
-                t.add_row(['x1s:'] + x1s[:length])
-                t.add_row(['x2s:'] + x2s[:length])
-                t.add_row(['x3s: '] + list(x3s[:length]))
-                print(t.draw())
-                print("ent_cnt:", ent_cnt)
-                print("************************")
-        return list(zip(x1s, x2s, x3s)
-
+from texttable import Texttable
+from tqdm import tqdm
 
 class DataModel(Dataset):
-
-    def __init__(self, opt, case='train'):
-        self.opt = opt
-        self.data_processer = DataProcessor(opt)
-        self.x = self.load_data(case)
-        # if case == 'train': self.x = self.x[:200]
-        # set_trace()
+    def __init__(self, npy_path=None):
+        self.x = self.load_data(npy_path)
 
     def __getitem__(self, idx):
         assert idx < len(self.x)
@@ -93,10 +15,48 @@ class DataModel(Dataset):
     def __len__(self):
         return len(self.x)
 
-    def load_data(self, case):
-        if case == 'train':
-            return self.data_processer.get_train_examples()
-        elif case == 'dev':
-            return self.data_processer.get_dev_examples()
+    def load_data(self, npy_path):
+        data = np.load(npy_path, allow_pickle=True)
+        return list(zip(data[0], data[1], data[2], data[3], data[4], data[5], data[6]))
 
-        return self.data_processer.get_test_examples()
+class DataProcessor(object):
+    def __init__(self, data_dir, vocab_path, label_path):
+        self.data_dir = data_dir
+        self.word2id = self._create_word_dict(vocab_path)
+        self.label2id = self._create_label_dict(label_path)
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_json(os.path.join(data_dir, "train.json")), 'train')
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_json(os.path.join(data_dir, "dev.json")), 'dev')
+
+    def get_test_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_json(os.path.join(data_dir, "test.json")), 'test')
+
+    def _create_label_dict(self, label_path):
+        """See base class."""
+        pass
+
+    def _create_examples(self, datas, case='train'):
+        """Creates examples for the training and dev sets."""
+        for data in tqdm(datas):
+            pass
+        # np.save(os.path.join(self.data_dir, case+'.npy'))
+
+    def _create_single_example(self, data, max_len):
+        pass
+
+if __name__ == '__main__':
+    data_processor = DataProcessor(data_dir='dataset/xxx',
+                                  vocab_path='/home/wpy/data/word2vec/bert-base-cased/vocab.txt',
+                                  label_path='your path to label path')
+    data_processor.get_train_examples('train raw path')
+    data_processor.get_dev_examples('dev raw path')
+    data_processor.get_test_examples('test raw path')
